bd3b4dfe76e7db73a8c8f250436d9cd2a5ba7df2
Professional <Professional@USER-PC>
2021-11-14 00:56:01 +0300

    Добавьте файлы проекта.

---
+++ Migrainebot/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Telegram.Bot;
+
+
+var token = "1447509072:AAHFaCW18mNcg1Y2PDPMwJrmasWok0JCAUw";
+
+
+var botClient = new TelegramBotClient(token);
+var me = await botClient.GetMeAsync();
+Console.WriteLine(
+  $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+);
+
+Console.WriteLine("Hello, World! "+token);
