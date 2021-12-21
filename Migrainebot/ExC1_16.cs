cb503880c252de46598926f3481d8eb6d4f95e67
Insomnia <olya_olya1998@mail.ru>
2021-12-22 02:54:46 +0300

    ExC1_16

---
+++ Migrainebot/ExC1_16.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrainebot
+{
+    //C1.16. Напечатать «столбиком» значения sin(2) , sin(3) , …, sin(20) .
+    internal class ExC1_16
+    {
+        public static void Run()
+        {
+            for (var i = 0; i <= 20; i++)
+            {
+                var number= Math.Sin(i);
+                Console.WriteLine($"{number}");
+            }
+        }
+    }
+
+}
