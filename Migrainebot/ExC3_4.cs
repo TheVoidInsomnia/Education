70402c909966431defe7d0f8ef66de65af02a338
TheVoidInsomni <olyaolya_1998@mail.ru>
2022-01-08 04:22:03 +0300

    ExC3_4

---
+++ Migrainebot/ExC3_4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrainebot
+{
+    //C3.4. Известны оценки абитуриента на четырех экзаменах.
+    //Определить сумму набранных им баллов.
+    internal class ExC3_4
+    {
+        public static void Run()
+        {
+            var sum=0;
+            int[] nums = { 20, 60, 80, 100 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
