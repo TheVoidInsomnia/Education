04101f9ebd67bfc1310e30b53eacab8d392146ab
TheVoidInsomni <olyaolya_1998@mail.ru>
2022-01-08 04:21:02 +0300

    ExC3_14

---
+++ Migrainebot/ExC3_14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrainebot
+{
+    //C3.14. Известны оценки по физике каждого из 20 учеников класса.
+    //Определить среднюю оценку.
+    internal class ExC3_14
+    {
+        public static void Run()
+        {
+            var sum = 0;
+            var arithmeticalMean=0;
+            int[] nums = {5, 4, 5, 4, 5, 3, 5, 4, 3, 2, 5, 3, 5, 4, 5, 3, 4, 5, 4, 3};
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                arithmeticalMean = sum / nums.Length;
+            }
+            Console.WriteLine($"{arithmeticalMean}");
+        }
+    }
+}
