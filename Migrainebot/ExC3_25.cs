d13d6466309aab44487cf496f31c91551918a60c
TheVoidInsomni <olyaolya_1998@mail.ru>
2022-01-08 04:22:24 +0300

    ExC3_25

---
+++ Migrainebot/ExC3_25.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrainebot
+{
+    //C3.25. В области 10 районов.
+    //Заданы площади, засеваемые пшеницей (в гектарах),
+    //и средняя урожайность (в центнерах с гектара) в каждом районе.
+    //Определить количество пшеницы, собранное в области, и среднюю урожайность по области.
+    internal class ExC3_25
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
